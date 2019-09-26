bd14472eb81c2d35645630cf5f5955345dd1d153
Bambuuh <lukas@klingberg.se>
2019-09-26 10:02:15 +0200

    Update PLACEHOLDER.controller.ts

--- express/Module/PLACEHOLDER.controller.ts
+++ express/Module/PLACEHOLDER.controller.ts
@@ -2,13 +2,13 @@ import { NextFunction, Response, Request } from 'express'
 
 import * as PLACEHOLDERService from './PLACEHOLDER.service'
 
-export async function getPLACEHOLDER(
+export async function getPASCAL_PLACEHOLDER(
   req: Request,
   res: Response,
   next: NextFunction
 ) {
   try {
-    const PLACEHOLDER = await PLACEHOLDERService.getPLACEHOLDER()
+    const PLACEHOLDER = await PLACEHOLDERService.getPASCAL_PLACEHOLDER()
     res.status(200).send(PLACEHOLDER)
   } catch (error) {
     next(error)
