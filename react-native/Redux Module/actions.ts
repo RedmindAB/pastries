078c1b301529cdf5029f7318463d28f83568b2c0
Sascha Ringström <sascha.ringstrom@redmind.se>
2019-10-03 14:27:25 +0200

    dumbed down redux module

--- react-native/Redux Module/actions.ts
+++ react-native/Redux Module/actions.ts
@@ -1,15 +1,4 @@
-import {
-  FETCH_UPPER_PLACEHOLDER,
-  CLEAR_UPPER_PLACEHOLDER,
-} from './types'
-
-export const fetchPASCAL_PLACEHOLDER = () => ({
-  type: FETCH_UPPER_PLACEHOLDER,
-  request: {
-    url: '',
-    method: 'GET'
-  }
-})
+import { CLEAR_UPPER_PLACEHOLDER } from './types'
 
 export const clearPASCAL_PLACEHOLDER = () => ({
   type: CLEAR_UPPER_PLACEHOLDER
